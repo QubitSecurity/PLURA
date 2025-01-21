475fd582cbb1b178c5eaceb170d7dbf00315243e
Qubit Security Inc. <plura@qubitsec.com>
2025-01-21 10:44:29 +0900

    Update background.js

--- functions/browser-extention/chrome/virustotal-search/script/background.js
+++ functions/browser-extention/chrome/virustotal-search/script/background.js
@@ -1,8 +1,23 @@
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.contextMenus.create({
-        id: "virusTotalSearch",
-        title: "바이러스토탈에서 '%s' 검색",
-        contexts: ["selection"]
+    // 브라우저의 기본 언어 설정 가져오기
+    chrome.i18n.getAcceptLanguages((languages) => {
+        let userLang = languages[0]; // 사용자의 첫 번째 언어 코드
+        let title;
+
+        if (userLang.startsWith("ko")) {
+            title = "바이러스토탈에서 '%s' 검색";
+        } else if (userLang.startsWith("ja")) {
+            title = "「VirusTotalで『%s』を検索」";
+        } else {
+            title = "Search for '%s' on VirusTotal";
+        }
+
+        // 우클릭 메뉴 생성
+        chrome.contextMenus.create({
+            id: "virusTotalSearch",
+            title: title,
+            contexts: ["selection"]
+        });
     });
 });
 
